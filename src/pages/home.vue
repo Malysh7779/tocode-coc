10d91faf9468b7ae145d0184eb6b77701062d1e1
Malysh7779 <aumalyshev@yandex.ru>
2024-09-07 19:57:32 +0500

    add img

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,26 @@
 <template>
-  <p>hello home</p>
+  <div class="mb-body">
+    <div class="card__wrapper" v-for="item in items" :key="item.id">
+      <Card :name="`${item.lvl} LVL`" :title="item.title" :imgUrl="item.img">
+        {{ item.descr }}
+      </Card>
+    </div>
+</div>
 </template>
+
+<script>
+import Card from '@/components/UI/Card'
+
+import items from '@/seeders/items.js'
+
+export default {
+  components: { 
+    Card 
+  },
+  data () {
+    return {
+      items: items
+    }
+  }
+}
+</script>
