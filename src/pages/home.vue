5920d4ba7f9dafead6456c1278d8725984b39092
Malysh7779 <aumalyshev@yandex.ru>
2024-09-08 17:28:41 +0500

    before hw

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,25 +1,70 @@
 <template>
-  <div class="mb-body">
-    <div class="card__wrapper" v-for="item in items" :key="item.id">
-      <Card :name="`${item.lvl} LVL`" :title="item.title" :imgUrl="item.img">
-        {{ item.descr }}
+  <carousel :settings="settings" :breakpoints="breakpoints">
+    <slide class="card__wrapper" v-for="item in items" :key="item.id">
+      <Card
+        :name="`${item.lvl} lvl`"
+        :title="item.title"
+        :imgUrl="item.img"
+        :link="`/${item.alias}`"
+      >
+        <template v-slot:body>
+          {{ item.descr }}
+        </template>
+        <template v-slot:footer>
+          <div class="card-stats">
+            <div
+              v-for="(stat, index) in item.info"
+              :key="index"
+              class="one-third"
+            >
+              <div class="stat-value">{{ stat.value }}</div>
+              <div class="stat">{{ stat.title }}</div>
+            </div>
+          </div>
+        </template>
       </Card>
-    </div>
-</div>
+    </slide>
+
+    <!-- addons -->
+    <template #addons>
+      <navigation />
+    </template>
+  </carousel>
 </template>
 
 <script>
-import Card from '@/components/UI/Card'
-
 import items from '@/seeders/items.js'
 
+import Card from '@/components/UI/Card'
+
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Navigation } from 'vue3-carousel'
+
 export default {
-  components: { 
-    Card 
+  components: {
+    Card,
+    Carousel,
+    Slide,
+    Navigation
   },
-  data () {
+  data() {
     return {
-      items: items
+      items: items,
+
+      // corousel
+      settings: {
+        itemsToShow: 2,
+        wrapAround: false,
+        snapAlign: 'center'
+      },
+      breakpoints: {
+        300: {
+          itemsToShow: 1
+        },
+        700: {
+          itemsToShow: 2
+        }
+      }
     }
   }
 }
